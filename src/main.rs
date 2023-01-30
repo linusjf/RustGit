12a7147426512ef029462fc9f306d3a34a129888
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-30 08:54:57 +0530

    feat: add head main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use shellexpand::tilde;
+use std::fs;
+use std::io;
+
+fn get_head() -> io::Result<String> {
+    fs::read_to_string(tilde("~/RustGit/.git/HEAD").to_string())
+}
+
+fn main() -> io::Result<()> {
+    let head = get_head()?;
+    println!("Head file: {:?}", head);
+    Ok(())
 }
